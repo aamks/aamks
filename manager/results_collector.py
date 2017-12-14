a149b81b9c3d113c15d5b788f65dc1a3046e86e4
Karol Kreński <mimoohowy@gmail.com>
2017-12-14 18:00:19 +0100

    animations repo

--- manager/results_collector.py
+++ manager/results_collector.py
@@ -1,1 +1,6 @@
-print("test")
+import os 
+import sys
+from subprocess import Popen,PIPE
+
+# gearman -f aOut "g1 /etc/fstab"
+Popen("scp {}:{} {}/vis/{}".format(sys.argv[1], sys.argv[2], os.environ['AAMKS_PROJECT'], os.path.basename(sys.argv[2])), shell=True)
