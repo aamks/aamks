e1b42450311d00c87f7bd00a19098c3e20b002ba
Karol Kreński <mimoohowy@gmail.com>
2018-02-06 10:41:27 +0100

    server howto

--- aamks.py
+++ aamks.py
@@ -14,7 +14,13 @@ OnInit()
 Geom()
 Path() # TODO: seems like we should call it elsewhere
 CfastTessellate()
-CfastMcarlo()
-EvacMcarlo()
-SmokeQuery("1")
+#CfastMcarlo()
+#EvacMcarlo()
+#SmokeQuery("1")
 OnEnd()
+
+# ssh mimooh@duch
+# aamks.<example>       # wystarczy raz 
+# AA                    # uruchom aamks
+# aamks                 # cd /usr/local/aamks
+# cd current            # current to taki akrasuski@consultrisk/risk/128/
