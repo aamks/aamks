4567c9fa02e823111959c2a002325d9026208841
akrasuski <adam.p.krasuski@gmail.com>
2018-04-20 15:31:46 +0200

    collecting distribution for pre-evacuation times

--- gui/interface/src/app/enums/risk-enums.ts
+++ gui/interface/src/app/enums/risk-enums.ts
@@ -125,88 +125,163 @@ export const RiskEnums =
         preTimes: [
             {
                 type: 'a',
-                preEvacuationRoomOfFireOrigin: [10, 0.5],
+                preEvacuationRoomOfFireOrigin: [29.85, 1.48],
                 pre: {
                     m1: {
-                        b1: { a1: { pre1: 30, pre2: 31 }, a2: { pre1: 60, pre2: 61 } },
-                        b2: { a1: { pre1: 40, pre2: 41 }, a2: { pre1: 70, pre2: 71 } },
-                        b3: { a1: { pre1: 50, pre2: 51 }, a2: { pre1: 80, pre2: 82 } }
+                        b1: { a1: { loc: 29.71, scale: 2.96 }, a2: { loc: 29.71, scale: 2.96 } },
+                        b2: { a1: { loc: 29.71, scale: 2.96 }, a2: { loc: 29.71, scale: 2.96 } },
+                        b3: { a1: { loc: 59.71, scale: 2.96 }, a2: { loc: 59.71, scale: 2.96 } }
                     },
                     m2: {
-                        b1: { a1: { pre1: 3, pre2: 6 }, a2: { pre1: 9, pre2: 12 } },
-                        b2: { a1: { pre1: 4, pre2: 7 }, a2: { pre1: 10, pre2: 13 } },
-                        b3: { a1: { pre1: 5, pre2: 8 }, a2: { pre1: 11, pre2: 14 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 59.42, scale: 5.92 }, a2: { loc: 59.42, scale: 5.92 } },
+                        b2: { a1: { loc: 59.42, scale: 5.92 }, a2: { loc: 59.42, scale: 5.92 } },
+                        b3: { a1: { loc: 89.42, scale: 5.92 }, a2: { loc: 89.42, scale: 5.92 } }
                     },
                     m3: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 899.42, scale: 5.92 }, a2: { loc: 899.42, scale: 5.92 } },
+                        b2: { a1: { loc: 899.42, scale: 5.92 }, a2: { loc: 899.42, scale: 5.92 } },
+                        b3: { a1: { loc: 929.42, scale: 5.92 }, a2: { loc: 929.42, scale: 5.92 }, a3: { loc: 929.42, scale: 5.92 } }
                     }
                 }
             },
             {
                 type: 'b',
-                preEvacuationRoomOfFireOrigin: [10, 0.5],
+                preEvacuationRoomOfFireOrigin: [59.85, 1.48],
                 pre: {
                     m1: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 60, pre2: 90 }, a2: { pre1: 60, pre2: 90 } }
+                        b1: { a1: { loc: 29.13, scale: 8.87 }, a2: { loc: 29.13, scale: 8.87 } },
+                        b2: { a1: { loc: 59.13, scale: 8.87 }, a2: { loc: 59.13, scale: 8.87 } },
+                        b3: { a1: { loc: 89.13, scale: 8.87 }, a2: { loc: 89.13, scale: 8.87 } }
                     },
                     m2: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 58.84, scale: 11.83 }, a2: { loc: 58.84, scale: 11.83 } },
+                        b2: { a1: { loc: 88.84, scale: 11.83 }, a2: { loc: 88.84, scale: 11.83 } },
+                        b3: { a1: { loc: 118.84, scale: 11.83 }, a2: { loc: 118.84, scale: 11.83 } }
                     },
                     m3: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 899.42, scale: 5.92 }, a2: { loc: 899.42, scale: 5.92 } },
+                        b2: { a1: { loc: 899.42, scale: 5.92 }, a2: { loc: 899.42, scale: 5.92 } },
+                        b3: { a1: { loc: 929.42, scale: 5.92 }, a2: { loc: 929.42, scale: 5.92 }, a3: { loc: 929.42, scale: 5.92 } }
                     }
                 }
             },
             {
                 type: 'c1',
-                preEvacuationRoomOfFireOrigin: [10, 0.5],
+                preEvacuationRoomOfFireOrigin: [59.85, 1.48],
                 pre: {
                     m1: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 60, pre2: 90 }, a2: { pre1: 60, pre2: 90 } }
+                        b1: { a1: { loc: 29.71, scale: 2.96 }, a2: { loc: 29.71, scale: 2.96 } },
+                        b2: { a1: { loc: 594.22, scale: 59.16 }, a2: { loc: 594.22, scale: 59.16 } },
+                        b3: { a1: { loc: 59.71, scale: 2.96 }, a2: { loc: 59.71, scale: 2.96 } }
                     },
                     m2: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 299.94, scale: 0.59 }, a2: { loc: 30, scale: 60 } },
+                        b2: { a1: { loc: 894.22, scale: 59.16 }, a2: { loc: 894.22, scale: 59.16 } },
+                        b3: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 30, scale: 60 } }
                     },
                     m3: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 594.22, scale: 59.16}},
+                        b2: { a1: { loc: 1199.94, scale: 0.59 }, a2: { loc: 1199.94, scale: 0.59 }, a3: { loc: 1199.94, scale: 0.59 }},
+                        b3: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 30, scale: 60 } }
                     }
                 }
             },
             {
                 type: 'c2',
-                preEvacuationRoomOfFireOrigin: [10, 0.5],
+                preEvacuationRoomOfFireOrigin: [59.85, 1.48],
                 pre: {
                     m1: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 60, pre2: 90 }, a2: { pre1: 60, pre2: 90 } }
+                        b1: { a1: { loc: 29.71, scale: 2.96 }, a2: { loc: 29.71, scale: 2.96 } },
+                        b2: { a1: { loc: 594.22, scale: 59.16 }, a2: { loc: 594.22, scale: 59.16 } },
+                        b3: { a1: { loc: 59.71, scale: 2.96 }, a2: { loc: 59.71, scale: 2.96 } }
                     },
                     m2: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 299.94, scale: 0.59 }, a2: { loc: 30, scale: 60 } },
+                        b2: { a1: { loc: 894.22, scale: 59.16 }, a2: { loc: 894.22, scale: 59.16 } },
+                        b3: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 30, scale: 60 } }
                     },
                     m3: {
-                        b1: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b2: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 } },
-                        b3: { a1: { pre1: 30, pre2: 60 }, a2: { pre1: 30, pre2: 60 }, a3: { pre1: 30, pre2: 60 } }
+                        b1: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 594.22, scale: 59.16}},
+                        b2: { a1: { loc: 1199.94, scale: 0.59 }, a2: { loc: 1199.94, scale: 0.59 }, a3: { loc: 1199.94, scale: 0.59 }},
+                        b3: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 30, scale: 60 } }
                     }
                 }
             },
-
+			{
+                type: 'c3',
+                preEvacuationRoomOfFireOrigin: [59.85, 1.48],
+                pre: {
+                    m1: {
+                        b1: { a1: { loc: 29.71, scale: 2.96 }, a2: { loc: 29.71, scale: 2.96 } },
+                        b2: { a1: { loc: 594.22, scale: 59.16 }, a2: { loc: 594.22, scale: 59.16 } },
+                        b3: { a1: { loc: 59.71, scale: 2.96 }, a2: { loc: 59.71, scale: 2.96 } }
+                    },
+                    m2: {
+                        b1: { a1: { loc: 299.94, scale: 0.59 }, a2: { loc: 30, scale: 60 } },
+                        b2: { a1: { loc: 894.22, scale: 59.16 }, a2: { loc: 894.22, scale: 59.16 } },
+                        b3: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 30, scale: 60 } }
+                    },
+                    m3: {
+                        b1: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 594.22, scale: 59.16}},
+                        b2: { a1: { loc: 1199.94, scale: 0.59 }, a2: { loc: 1199.94, scale: 0.59 }, a3: { loc: 1199.94, scale: 0.59 }},
+                        b3: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 30, scale: 60 } }
+                    }
+                }
+            },
+			{
+                type: 'd1',
+                preEvacuationRoomOfFireOrigin: [59.85, 1.48],
+                pre: {
+                    m1: {
+                        b1: { a1: { loc: 28.84, scale: 11.83 }, a2: { loc: 28.84, scale: 11.83 } },
+                        b2: { a1: { loc: 59.13, scale: 8.87 }, a2: { loc: 59.13, scale: 8.87 } },
+                        b3: { a1: { loc: 89.13, scale: 8.87 }, a2: { loc: 89.13, scale: 8.87 } }
+                    },
+                    m2: {
+                        b1: { a1: { loc: 58.84, scale: 11.83 }, a2: { loc: 58.84, scale: 11.83 } },
+                        b2: { a1: { loc: 894.22, scale: 59.16 }, a2: { loc: 894.22, scale: 59.16 } },
+                        b3: { a1: { loc: 30, scale: 60 }, a2: { loc: 30, scale: 60 }, a3: { loc: 30, scale: 60 } }
+                    },
+                    m3: {
+                        b1: { a1: { loc: 899.94, scale: 0.59 }, a2: { loc: 899.94, scale: 0.59 }, a3: { loc: 899.94, scale: 0.59 } },
+                        b2: { a1: { loc: 1199.94, scale: 0.59 }, a2: { loc: 1199.94, scale: 0.59 }, a3: { loc: 1199.94, scale: 0.59 }},
+                        b3: { a1: { loc: 1259.94, scale: 0.59 }, a2: { loc: 1259.94, scale: 0.59 }, a3: { loc: 1259.94, scale: 0.59 }}
+                    }
+                }
+            },
+			{
+                type: 'd2',
+                preEvacuationRoomOfFireOrigin: [59.85, 1.48],
+                pre: {
+                pre: {
+                    m1: {
+                        b2: { a1: { loc: 297.11, scale: 29.56 }, a2: { loc: 297.11, scale: 29.56 } },
+                        b3: { a1: { loc: 357.11, scale: 29.58 }, a2: { loc: 357.11, scale: 29.58 } }
+                    },
+                    m2: {
+                        b2: { a1: { loc: 594.22, scale: 59.16 }, a2: { loc: 594.22, scale: 59.16 } },
+                        b3: { a1: { loc: 654.22, scale: 59.16 }, a2: { loc: 654.22, scale: 59.16 } }
+                    },
+                    m3: {
+                        b2: { a1: { loc: 594.22, scale: 59.16 }, a2: { loc: 594.22, scale: 59.16 } },
+                        b3: { a1: { loc: 654.22, scale: 59.16 }, a2: { loc: 654.22, scale: 59.16 }, a3: { loc: 654.22, scale: 59.16 } }
+                    }
+                }
+            },
+			{
+                type: 'e',
+                preEvacuationRoomOfFireOrigin: [59.85, 1.48],
+                pre: {
+                    m1: {
+                        b3: { a1: { loc: 88.56, scale: 14.79 }, a2: { loc: 88.56, scale: 14.79 } }
+                    },
+                    m2: {
+                        b3: { a1: { loc: 118.27, scale: 17.75 }, a2: { loc: 118.27, scale: 17.75 } }
+                    },
+                    m3: {
+                        b3: { a1: { loc: 899.94, scale: 0.59 }, a2: { loc: 899.94, scale: 0.59 }, a3: { loc: 899.94, scale: 0.59 } }
+                    }
+                }
+            },
         ]
     };
