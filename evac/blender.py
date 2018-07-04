88a245120455174f939cf2eb46d1f25f355a4266
Karol Kreński <mimoohowy@gmail.com>
2018-07-04 15:04:49 +0200

    blender navmesh

--- evac/blender.py
+++ evac/blender.py
@@ -1,4 +1,5 @@
 import bpy
+import bmesh
 import json
 import os
 from include import Sqlite
@@ -8,7 +9,7 @@ from include import Colors
 
 class BlenderAamksEvac():
     ''' 
-    Blender for development debuging.
+    Blender for development debuging. Blender produces navmesh triangles.
     '''
 
     def __init__(self):# {{{
@@ -18,13 +19,10 @@ class BlenderAamksEvac():
         self._navmesh_collector=[]
         self._init_blender()
         self._make_materials()
-        #self._make_exit()
         self._make_rooms()
         self._make_doors()
         self._cut_doors()
-        #self._make_stairs()
         self._make_navmesh()
-        #self._make_evacuees()
 # }}}
     def _newest_sim_id(self):# {{{
         self._sim_id=os.popen("ls -t {}/workers | grep -v vis | head -n 1".format(os.environ['AAMKS_PROJECT'])).read().strip()
@@ -35,8 +33,6 @@ class BlenderAamksEvac():
 # }}}
 
     def _make_materials(self):# {{{
-        for material in bpy.data.materials:
-            bpy.data.materials.remove(material)
         c=Colors()
         self.materials=dict()
         colors=self.json.read("{}/geom/colors.json".format(os.environ['AAMKS_PATH']))['darkColors']
@@ -48,10 +44,6 @@ class BlenderAamksEvac():
                 mat.alpha=0.4
             self.materials[k]=mat
 
-# }}}
-    def _make_exit(self):# {{{
-        bpy.ops.mesh.primitive_cube_add(location=(45,23,2))
-        bpy.context.object.name='00_EXIT'
 # }}}
     def _make_rooms(self):# {{{
         ''' 0.001 prevents z-fighting of overlaping polygons. '''
@@ -92,10 +84,9 @@ class BlenderAamksEvac():
 
         bpy.ops.mesh.primitive_cube_add(location=origin)
         bpy.ops.transform.resize(value=size)
-        smaller=bpy.context.object
-
-        bpy.ops.object.duplicate_move()
         bigger=bpy.context.object
+        smaller=bigger.copy()
+        smaller.data=bigger.data.copy()
 
         bigger.select=False
         smaller.select=True
@@ -121,27 +112,6 @@ class BlenderAamksEvac():
         bpy.ops.object.delete()
 
 # }}}
-    def _make_stairs(self):# {{{
-        try:
-            stairs=json.loads(self.s.query("SELECT json FROM staircaser")[0]['json'])
-        except:
-            return
-        stairs_names=[]
-        for num,i in enumerate(stairs):
-            name="stair_{}".format(num)
-            bpy.ops.mesh.primitive_cube_add(location=i['center'])
-            bpy.ops.transform.resize(value=i['size'])
-            bpy.context.object.name=name
-            stairs_names.append(name)
-
-        bpy.ops.object.select_all(action='DESELECT')
-        for i in stairs_names:
-            bpy.data.objects[i].select=True
-        bpy.ops.object.join()
-        bpy.context.object.name='stairs'
-        self._navmesh_collector.append('stairs')
-
-# }}}
     def _cut_doors(self):# {{{
         bpy.ops.object.select_all(action='DESELECT')
         for compa in self.s.query("SELECT name FROM aamks_geom WHERE type_pri='COMPA' ORDER BY name"): 
@@ -165,80 +135,27 @@ class BlenderAamksEvac():
         bpy.ops.object.select_all(action='DESELECT')
         for i in self._navmesh_collector:
             bpy.data.objects[i].select=True
-        bpy.ops.object.duplicate_move()
+        bpy.ops.object.duplicate()
         bpy.ops.object.join()
         navmesh_input=bpy.context.object
         bpy.ops.mesh.navmesh_make()
         bpy.ops.object.select_all(action='DESELECT')
         navmesh_input.select=True
         bpy.ops.object.delete()
-        #bpy.data.objects['Navmesh'].hide=True
 
-# }}}
-    def _single_evacuee(self,name,pos,mat):# {{{
-        agent_height=1.6
-        bpy.ops.mesh.primitive_cylinder_add(radius=0.25, depth=agent_height, location=(pos[0], pos[1], pos[2]+0.5*agent_height*1.1))
-        obj=bpy.context.object
-        obj.data.materials.append(mat)
-        obj.name=name
-        obj.game.use_obstacle_create=True
-        obj.game.obstacle_radius=0.27
-        obj.game.physics_type='DYNAMIC'
-        obj.game.radius=0.27
-        #obj.game.elasticity=0.9
+        current_obj = bpy.context.active_object  
+          
+        collect=''
+        for face in current_obj.data.polygons:  
+            for vert in face.vertices:
+                local_point = current_obj.data.vertices[vert].co
+                collect+="{};{};".format(local_point[0], local_point[1])
+            collect+="\n"
 
-        sensors = obj.game.sensors
-        controllers = obj.game.controllers
-        actuators = obj.game.actuators
+        with open("/home/mimooh/nav.txt", "w") as f: 
+            f.write(collect)
+        print(collect)
 
-        logic=bpy.ops.logic
-        logic.sensor_add(type='MOVEMENT', name=name)
-        logic.actuator_add(type='STEERING', name=name)
-        bpy.ops.logic.controller_add(object=obj.name)
-
-        # Newly added logic blocks will be the last ones:
-        sensor = sensors[-1]
-        controller = controllers[-1]
-        actuator = actuators[-1]
-        sensor.axis='ALLAXIS'
-        actuator.mode='PATHFOLLOWING'
-        actuator.target = bpy.data.objects["00_EXIT"]
-        actuator.navmesh= bpy.data.objects["Navmesh"]
-        actuator.self_terminated= True
-        actuator.show_visualization= True
-        actuator.acceleration=5
-        actuator.velocity=15
-        actuator.distance=6
-
-        sensor.link(controller)
-        actuator.link(controller)
-
-        return name
-# }}}
-    def _make_evacuees(self):# {{{
-        ''' Create from materials and coords. '''
-
-        mat = bpy.data.materials.new("white")
-        mat.diffuse_color = (1,1,1)
-
-        self.evacuees=[]
-        floors_data=self.json.read("{}/workers/{}/evac.json".format(os.environ['AAMKS_PROJECT'], self._sim_id))['FLOORS_DATA']
-        for floor,data in floors_data.items():
-            for name,setup in data['EVACUEES'].items():
-                self._single_evacuee(name, setup['ORIGIN'], mat)
-                self.evacuees.append(name)
-# }}}
-
-# clear belongs to blender window# {{{
-# import bpy
-# suzanne=bpy.data.objects['Suzanne']
-# bpy.ops.object.select_all(action='SELECT')
-# suzanne.select=False
-# bpy.ops.object.delete()
-# bpy.context.scene.objects.active = suzanne
-
-# separate
-# filename="{}/evac/blender.py".format(os.environ['AAMKS_PATH'])
 # }}}
 
 BlenderAamksEvac()
