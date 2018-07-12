cf7438a5d291bce83dadfc397979c216262d7657
Karol Kreński <mimoohowy@gmail.com>
2018-07-12 17:00:16 +0200

    mimooh@d

--- geom/apainter/js/apainter.js
+++ geom/apainter/js/apainter.js
@@ -1,42 +1,62 @@
 $(function()  { 
+	var canvas=[1100,600];
 	var names=[];
-	var colors={ ROOM:"#282828" , D:"#888800" , COR:"#002233" , HALL:"#00300a" , W:"#006aa4" , HOLE:"#008800" , STAI:"#280033" , VNT:"#aa00aa" , C:"#883d00"  , E:"#660000"    };
-	var letters={ ROOM:"r"      , D:"d"       , COR:"q"       , HALL:"a"       , W:"w"       , HOLE:"x"       , STAI:"s"       , VNT:"v"       , C:"c"        , E:"e" };
-	var anames={ ROOM:"Room"     , D:"Door"    , COR:"Corr"    , HALL:"Hall"    , W:"Win"     , HOLE:"Hole"    , STAI:"Stair"   , VNT:"Vent"    , E:"D.Electr" , C:"D.Closr" };
+	var colors={ ROOM:"#006694" , COR:"#002244" , D:"#888800" , HALL:"#00300a" , W:"#55aaff" , HOLE:"#008800" , STAI:"#280033" , C:"#883d00"  , E:"#660000" , VNT:"#aa00aa"   };
+	var letters={ ROOM:"r"      , COR:"q"       , D:"d"       , HALL:"a"       , W:"w"       , HOLE:"x"       , STAI:"s"       , C:"c"        , E:"e"       , VNT:"v" };
+	var anames={ ROOM:"Room"    , COR:"Corr"    , D:"Door"    , HALL:"Hall"    , W:"Win"     , HOLE:"Hole"    , STAI:"Stair"   , E:"D.Electr" , C:"D.Closr" , VNT:"Vvent"   };
 
 	var geom_counter=0;
 	var collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
 	d3.select('body').append('nav');
 	d3.select('body').append('legend');
-	make_legend();
-	var svg = d3.select('body').append('svg').attr("width", 1200).attr("height", 600);
+	var svg = d3.select('body').append('svg').attr("width", canvas[0]).attr("height", canvas[1]);
 	var g = svg.append("g");
+	make_legend();
 
 // zoomer//{{{
 	svg.append("rect")
 		.attr("id", 'zoomer')
-		.attr("width", 1200)
-		.attr("height", 600)
-		.style("fill", "none")
-		.style("pointer-events", "all")
+		.attr("width", canvas[0])
+		.attr("height", canvas[1])
+		.attr("fill", "#aa4400")
+		.attr("opacity", 0)
+		.attr("pointer-events", "visible")
+		.attr("visibility", "hidden")
 		.call(d3.zoom()
 			.scaleExtent([1 / 2, 4])
+			.filter(function(){
+			return (event.button === 0 ||
+					  event.button === 1);
+			})
 			.on("zoom", zoomed));
 	function zoomed() {
 		  g.attr("transform", d3.event.transform);
 	}
 //}}}
-	function make_legend() { //{{{
-		for(var key in colors) {
-			$('legend').append("<div class=legend style='background-color: "+colors[key]+"'>"+letters[key]+" "+anames[key]+"</div>");
-		}
-	}
-
+// image//{{{
+	g.append("svg:image")
+		.attr("id", 'img')
+		.attr("x", 0)
+		.attr("y", 0)
+		.attr("opacity", 0.3)
+		.attr("xlink:href", "/apainter/gfx.jpg");
 //}}}
-
-	$(this).keypress((e) => { //{{{
+// keyboard//{{{
+	$(this).keypress((e) => { 
 		for(var key in letters) {
 			if (e.key == letters[key]) { rectCreate(colors[key], letters[key]); }
+		}
+	});
+
+	$(this).keydown((e) => { 
+		if (e.key == 'Control') { 
+			$("#zoomer").attr("visibility", "visible");
+		}
+	});
+
+	$(this).keyup((e) => { 
+		if (e.key == 'Control') { 
+			$("#zoomer").attr("visibility", "hidden");
 		}
 	});
 //}}}
@@ -54,36 +74,33 @@ $(function()  {
 //}}}
 
 	$('body').click(function(evt){
-		console.log("id", evt.target.id);
-		// if (evt.target.id != 'zoomer') { 
-		// 	$("#"+evt.target.id).css('opacity', 0.2);
-		// }
-	});
-	
-	svg.on('mouseup', function() {
-		svg.on('mousedown', null);
-		svg.on('mousemove', null);
-		$("#zoomer").css("pointer-events", "all");
+		if (evt.target.id != '' && evt.target.id != 'zoomer') { 
+			d3.selectAll('rect').style('fill-opacity', 0.4);
+			$("#"+evt.target.id).css('fill-opacity', 0.1);
+		}
 	});
 	
 	function rectCreate(color, geom) {//{{{
 		var self = this;
 		var mouse;
-		$("#zoomer").css("pointer-events", "none");
 
 		svg.on('mousedown', function() {
 			mouse=d3.mouse(this);
 			name=update_nav(geom);
 			self.name = name
 			self.rectData = [ { x: mouse[0], y: mouse[1] }, { x: mouse[0], y: mouse[1] } ];
-			d3.selectAll('rect').style('opacity', 0.4);
-			self.rect=g.append('rect').attr('id', name).attr('opacity', 0.4).attr('fill', color).attr('class', 'rectangle');
+			self.rect=g.append('rect').attr('id', name).attr('fill-opacity',0.4).attr('fill', color).attr('stroke-width', 1).attr('stroke', color).attr('class', 'rectangle');
 
 			svg.on('mousemove', function() {
 				mouse = d3.mouse(this);
 				self.rectData[1] = { x: mouse[0], y: mouse[1] };
 				updateRect();
 			});  
+		});
+
+		svg.on('mouseup', function() {
+			svg.on('mousedown', null);
+			svg.on('mousemove', null);
 		});
 
 		function updateRect() {  
@@ -95,8 +112,16 @@ $(function()  {
 			});   
 
 		}
-		
 	}
+
+	function make_legend() { //{{{
+		for(var key in colors) {
+			$('legend').append("<div class=legend style='background-color: "+colors[key]+"'>"+letters[key]+" "+anames[key]+"</div>");
+		}
+		$('legend').append(" ctrl+middlemouse: zoom/drag, letters to draw");
+	}
+
+//}}}
 //}}}
 
 });
