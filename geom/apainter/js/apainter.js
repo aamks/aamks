e6a9d3b3a348829110080dffbc9bdbee98017ffc
Karol Kreński <mimoohowy@gmail.com>
2018-07-13 00:34:36 +0200

    mimooh@d

--- geom/apainter/js/apainter.js
+++ geom/apainter/js/apainter.js
@@ -1,17 +1,17 @@
 $(function()  { 
 	var canvas=[1100,600];
-	var names=[];
+	var db=TAFFY(); // http://taffydb.com/working_with_data.html
+	var selected_rect='';
 	var colors={ ROOM:"#006694" , COR:"#002244" , D:"#888800" , HALL:"#00300a" , W:"#55aaff" , HOLE:"#008800" , STAI:"#280033" , C:"#883d00"  , E:"#660000" , VNT:"#aa00aa"   };
-	var letters={ ROOM:"r"      , COR:"q"       , D:"d"       , HALL:"a"       , W:"w"       , HOLE:"x"       , STAI:"s"       , C:"c"        , E:"e"       , VNT:"v" };
+	var letters={ ROOM:"r"      , COR:"q"       , D:"d"       , HALL:"a"       , W:"w"       , HOLE:"z"       , STAI:"s"       , C:"c"        , E:"e"       , VNT:"v" };
 	var anames={ ROOM:"Room"    , COR:"Corr"    , D:"Door"    , HALL:"Hall"    , W:"Win"     , HOLE:"Hole"    , STAI:"Stair"   , E:"D.Electr" , C:"D.Closr" , VNT:"Vvent"   };
 
-	var geom_counter=0;
-	var collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'});
-	d3.select('body').append('nav');
 	d3.select('body').append('legend');
 	var svg = d3.select('body').append('svg').attr("width", canvas[0]).attr("height", canvas[1]);
-	var g = svg.append("g");
+	var g = svg.append("g").attr("id", "g_aamks");
 	make_legend();
+	d3.select('body').append('br');
+	d3.select('body').append('status');
 
 // zoomer//{{{
 	svg.append("rect")
@@ -44,7 +44,7 @@ $(function()  {
 // keyboard//{{{
 	$(this).keypress((e) => { 
 		for(var key in letters) {
-			if (e.key == letters[key]) { rectCreate(colors[key], letters[key]); }
+			if (e.key == letters[key]) { rect_create(colors[key], letters[key]); }
 		}
 	});
 
@@ -59,34 +59,38 @@ $(function()  {
 			$("#zoomer").attr("visibility", "hidden");
 		}
 	});
-//}}}
-	function update_nav(geom) { //{{{
-		geom_counter++;
-		name=geom+"_"+geom_counter;
-		names.push(name);
-		names.sort(collator.compare);
-		$("nav").html('');
-		for (var i in names) { 
-			$("nav").append("<name data-id="+names[i]+">"+names[i]+"</name><br>");
-		}
-		return name;
-	}
-//}}}
 
+//}}}
+// delete//{{{
 	$('body').click(function(evt){
-		if (evt.target.id != '' && evt.target.id != 'zoomer') { 
+		if (evt.target.id != '' && ( evt.target.id != 'g_aamks' && evt.target.id != 'zoomer' && evt.target.id != 'img' )) { 
 			d3.selectAll('rect').style('fill-opacity', 0.4);
 			$("#"+evt.target.id).css('fill-opacity', 0.1);
+			selected_rect=evt.target.id;
+			var x=db({'name':selected_rect}).select("name", "id", "geom", "x", "y");
+			$("status").html("INFO: "+JSON.stringify(x));
+			console.log("selected", selected_rect);
+		}
+		if (evt.target.id != '' && ( evt.target.id == 'zoomer' || evt.target.id == 'img' )) { 
+			d3.selectAll('rect').style('fill-opacity', 0.4);
+			selected_rect=''
 		}
 	});
-	
-	function rectCreate(color, geom) {//{{{
+
+	$(this).keypress((e) => { 
+		if (e.key == 'x' && selected_rect != "") { 
+			$("#"+selected_rect).remove();
+			db({"name":selected_rect}).remove();
+		}
+	});
+	//}}}
+	function rect_create(color, geom) {//{{{
 		var self = this;
 		var mouse;
 
 		svg.on('mousedown', function() {
 			mouse=d3.mouse(this);
-			name=update_nav(geom);
+			name=db_insert(geom);
 			self.name = name
 			self.rectData = [ { x: mouse[0], y: mouse[1] }, { x: mouse[0], y: mouse[1] } ];
 			self.rect=g.append('rect').attr('id', name).attr('fill-opacity',0.4).attr('fill', color).attr('stroke-width', 1).attr('stroke', color).attr('class', 'rectangle');
@@ -113,15 +117,23 @@ $(function()  {
 
 		}
 	}
-
+//}}}
 	function make_legend() { //{{{
 		for(var key in colors) {
 			$('legend').append("<div class=legend style='background-color: "+colors[key]+"'>"+letters[key]+" "+anames[key]+"</div>");
 		}
-		$('legend').append(" ctrl+middlemouse: zoom/drag, letters to draw");
+		$('legend').append(" ctrl+middlemouse: zoom/drag, letters draw, x deletes selected");
 	}
 
 //}}}
+	function db_insert(geom) { //{{{
+		var next=db({'geom':geom}).max("id") + 1;
+		var name=geom+"_"+next;
+		db.insert({"id": next, "geom": geom, "name": name, "x": "100.0", "y": "100.0" });
+		var x=db().select("name");
+		$("status").html(JSON.stringify(x));
+		return name; 
+	}
 //}}}
 
 });
