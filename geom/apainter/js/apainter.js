d5b88eaa8f83d57a5e277a114294dd60f9adabb3
Karol Kreński <mimoohowy@gmail.com>
2018-07-13 21:10:06 +0200

    apainter axes

--- geom/apainter/js/apainter.js
+++ geom/apainter/js/apainter.js
@@ -1,40 +1,43 @@
 $(function()  { 
-	var canvas=[1100,600];
+	var canvas=[screen.width*0.99,screen.height-300];
 	var db=TAFFY(); // http://taffydb.com/working_with_data.html
 	var selected_rect='';
+	var zt={'x':0, 'y':0, 'k':1}; // zoom transform
 	var colors={ ROOM:"#006694" , COR:"#002244" , D:"#888800" , HALL:"#00300a" , W:"#55aaff" , HOLE:"#008800" , STAI:"#280033" , C:"#883d00"  , E:"#660000" , VNT:"#aa00aa"   };
 	var letters={ ROOM:"r"      , COR:"q"       , D:"d"       , HALL:"a"       , W:"w"       , HOLE:"z"       , STAI:"s"       , C:"c"        , E:"e"       , VNT:"v" };
 	var anames={ ROOM:"Room"    , COR:"Corr"    , D:"Door"    , HALL:"Hall"    , W:"Win"     , HOLE:"Hole"    , STAI:"Stair"   , E:"D.Electr" , C:"D.Closr" , VNT:"Vvent"   };
-
-	d3.select('body').append('legend');
-	var svg = d3.select('body').append('svg').attr("width", canvas[0]).attr("height", canvas[1]);
-	var g = svg.append("g").attr("id", "g_aamks");
-	make_legend();
-	d3.select('body').append('br');
-	d3.select('body').append('status');
+	var svg;
+	var g_aamks;
+	var ax={};
+	site();
 
 // zoomer//{{{
 	svg.append("rect")
 		.attr("id", 'zoomer')
 		.attr("width", canvas[0])
 		.attr("height", canvas[1])
-		.attr("fill", "#aa4400")
+		.attr("fill", "#ax4400")
 		.attr("opacity", 0)
 		.attr("pointer-events", "visible")
 		.attr("visibility", "hidden")
 		.call(d3.zoom()
-			.scaleExtent([1 / 2, 4])
+			.scaleExtent([1 / 10, 40])
 			.filter(function(){
 			return (event.button === 0 ||
 					  event.button === 1);
 			})
+			.translateExtent([[-10000, -10000], [10000 , 10000]])
 			.on("zoom", zoomed));
 	function zoomed() {
-		  g.attr("transform", d3.event.transform);
+		g_aamks.attr("transform", d3.event.transform);
+		zt = d3.event.transform;
+		ax.gX.call(ax.xAxis.scale(d3.event.transform.rescaleX(ax.x)));
+		ax.gY.call(ax.yAxis.scale(d3.event.transform.rescaleY(ax.y)));
+
 	}
 //}}}
 // image//{{{
-	g.append("svg:image")
+	g_aamks.append("svg:image")
 		.attr("id", 'img')
 		.attr("x", 0)
 		.attr("y", 0)
@@ -61,18 +64,18 @@ $(function()  {
 	});
 
 //}}}
-// delete//{{{
+// selected //{{{
 	$('body').click(function(evt){
 		if (evt.target.id != '' && ( evt.target.id != 'g_aamks' && evt.target.id != 'zoomer' && evt.target.id != 'img' )) { 
-			d3.selectAll('rect').style('fill-opacity', 0.4);
-			$("#"+evt.target.id).css('fill-opacity', 0.1);
+			d3.selectAll('rect').attr('fill-opacity', 0.4);
+			$("#"+evt.target.id).attr('fill-opacity', 0.1);
 			selected_rect=evt.target.id;
 			var x=db({'name':selected_rect}).select("name", "id", "geom", "x", "y");
 			$("status").html("INFO: "+JSON.stringify(x));
 			console.log("selected", selected_rect);
 		}
 		if (evt.target.id != '' && ( evt.target.id == 'zoomer' || evt.target.id == 'img' )) { 
-			d3.selectAll('rect').style('fill-opacity', 0.4);
+			d3.selectAll('rect').attr('fill-opacity', 0.4);
 			selected_rect=''
 		}
 	});
@@ -87,17 +90,21 @@ $(function()  {
 	function rect_create(color, geom) {//{{{
 		var self = this;
 		var mouse;
+		var x0;
+		var y0;
 
 		svg.on('mousedown', function() {
 			mouse=d3.mouse(this);
 			name=db_insert(geom);
+			x0=(mouse[0]-zt.x)/zt.k;
+			y0=(mouse[1]-zt.y)/zt.k;
 			self.name = name
-			self.rectData = [ { x: mouse[0], y: mouse[1] }, { x: mouse[0], y: mouse[1] } ];
-			self.rect=g.append('rect').attr('id', name).attr('fill-opacity',0.4).attr('fill', color).attr('stroke-width', 1).attr('stroke', color).attr('class', 'rectangle');
+			self.rectData = [ { x: x0, y: y0 }, { x: x0, y: y0 } ];
+			self.rect=g_aamks.append('rect').attr('id', name).attr('fill-opacity',0.4).attr('fill', color).attr('stroke-width', 1).attr('stroke', color).attr('class', 'rectangle');
 
 			svg.on('mousemove', function() {
 				mouse = d3.mouse(this);
-				self.rectData[1] = { x: mouse[0], y: mouse[1] };
+				self.rectData[1] = { x: (mouse[0]-zt.x)/zt.k, y: (mouse[1]-zt.y)/zt.k };
 				updateRect();
 			});  
 		});
@@ -118,7 +125,7 @@ $(function()  {
 		}
 	}
 //}}}
-	function make_legend() { //{{{
+	function legend() { //{{{
 		for(var key in colors) {
 			$('legend').append("<div class=legend style='background-color: "+colors[key]+"'>"+letters[key]+" "+anames[key]+"</div>");
 		}
@@ -135,5 +142,44 @@ $(function()  {
 		return name; 
 	}
 //}}}
+function axes() { //{{{
+	ax.x = d3.scaleLinear()
+		.domain([-1, canvas[0]+ 1])
+		.range([-1, canvas[0]+ 1 ]);
+
+	ax.y = d3.scaleLinear()
+		.domain([-1, canvas[1] + 1])
+		.range([-1, canvas[1] + 1]);
+
+	ax.xAxis = d3.axisBottom(ax.x)
+		.ticks(10)
+		.tickSize(canvas[1])
+		.tickPadding(8 - canvas[1]);
+
+	ax.yAxis = d3.axisRight(ax.y)
+		.ticks(4)
+		.tickSize(canvas[0])
+		.tickPadding(8 - canvas[0]);
+
+	ax.gX = svg.append("g")
+		.attr("class", "axis axis--x")
+		.call(ax.xAxis);
+
+	ax.gY = svg.append("g")
+		.attr("class", "axis axis--y")
+		.call(ax.yAxis);
+}
+//}}}
+function site() { //{{{
+	d3.select('body').append('legend');
+	svg = d3.select('body').append('svg').attr("width", canvas[0]).attr("height", canvas[1]);
+	axes();
+	g_aamks = svg.append("g").attr("id", "g_aamks");
+	legend();
+	d3.select('body').append('br');
+	d3.select('body').append('status');
+}
+//}}}
+
 
 });
