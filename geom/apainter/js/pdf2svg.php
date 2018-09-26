4ad3f410195f0635aff36177c8e135ca001d1347
Karol Kreński <mimoohowy@gmail.com>
2018-09-26 14:51:58 +0200

    pdf2svg

--- geom/apainter/js/pdf2svg.php
+++ geom/apainter/js/pdf2svg.php
@@ -4,6 +4,9 @@ header('Access-Control-Allow-Origin: *');
 $in=$_FILES['file']['tmp_name'];
 $z=shell_exec("pdf2svg $in out.svg 2>&1");
 $svg='';
-if(empty($z)) { $svg=shell_exec("cat out.svg"); }
+if(empty($z)) { 
+	$svg=shell_exec("cat out.svg"); 
+	$svg=preg_replace("/#/", "%23", $svg);
+}
 echo json_encode(array("err"=>$z, "svg"=>$svg));
 ?>
