b1b73fdc26026d348cfacfae213fdcb37e859fb0
Karol Kreński <mimoohowy@gmail.com>
2018-09-26 14:32:14 +0200

    i2

--- gui/i2/i2.php
+++ gui/i2/i2.php
@@ -5,14 +5,14 @@ require_once("inc.php") ;
 function menu() { /*{{{*/
 	echo "
 	<img width=160 src=logo.svg><br><br><br>
-	<a href=/apainter class=blink>Create geometry</a><br>
-	<a href=/aamks/workers/vis/master.html class=blink>Visualization</a><br>
+	<a href=/i2/apainter class=blink>Create geometry</a><br>
+	<a href=/i2/workers/vis/master.html class=blink>Visualization</a><br>
 	";
 }
 /*}}}*/
 function main() { /*{{{*/
 	if(empty($_SESSION['nn'])) { $_SESSION['nn']=new Aamks("Aamks") ; }
-	$_SESSION['nn']->htmlHead("aamks");
+	$_SESSION['nn']->htmlHead("i2");
 	$_SESSION['nn']->logoutButton();
 	menu();
 }
