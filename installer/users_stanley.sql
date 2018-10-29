4b5ff834370bcf22ada0abcda865c02ed80c80b4
stanley998 <34373901+stanley998@users.noreply.github.com>
2018-10-29 15:07:58 +0100

    mimooh@szach

---
+++ installer/users_stanley.sql
@@ -0,0 +1,17 @@
+\c aamks
+drop table nusers;
+CREATE TABLE nusers (---{{{
+	id serial PRIMARY KEY, 
+	google_id text,
+    userName text,
+    email text UNIQUE,
+    password text,
+	picture text,
+	access_time timestamp,
+	activation_token text,
+	created timestamp default current_timestamp
+);
+---}}}
+ALTER TABLE nusers OWNER TO aamks;
+GRANT ALL ON TABLE nusers TO aamks;
+GRANT ALL ON SEQUENCE nusers_id_seq TO aamks;
