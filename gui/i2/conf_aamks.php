43123d33883ea2be063a05522839298ba207645b
Karol Kreński <mimoohowy@gmail.com>
2018-11-29 17:11:32 +0100

    i2 form: params

--- gui/i2/conf_aamks.php
+++ gui/i2/conf_aamks.php
@@ -171,7 +171,6 @@ require_once("inc.php");
 }
 */
 /*}}}*/
-
 function read_json($json_path) { /*{{{*/
 	$conf=json_decode('
 	{
@@ -180,35 +179,36 @@ function read_json($json_path) { /*{{{*/
 			"scenario_id": 1,
 			"number_of_simulations": 1,
 			"simulation_time": 100,
-			"indoor_temperature":  { "min": -5, "max": 10 },
-			"outdoor_temperature": { "min": -5, "max": 10 },
+			"indoor_temperature":  [ { "min": -5, "max": 10 } ],
+			"outdoor_temperature": [ { "min": -5, "max": 10 } ],
 			"indoor_pressure": 101325,
 			"geometry_type": "office1",
 			"material": [ 
 				{ "ceiling": "concrete" , "thickness" : 0.3 } ,
 				{ "floor":   "concrete" , "thickness" : 0.3 } ,
 				{ "wall":     "brick"   , "thickness" : 0.3 } 
-			]
-		} ,
-
-		"advanced": {
+			],
 			"detectors": false,
 			"detectors_type": "heat",
-			"detectors_temp":   { "mean": 68, "sd": 5 },
-			"detectors_obscur": { "mean": 0, "sd": 5 },
+			"detectors_temp":   [ { "mean": 68, "sd": 5 } ],
+			"detectors_obscur": [ { "mean": 0, "sd": 5 } ],
 			"detectors_not_broken": 0.96,
 
 			"sprinklers": false,
-			"sprinklers_temp":    { "mean": 68, "sd": 3 },
-			"sprinklers_density": { "mean": 0.0005, "sd": 0.0001 },
+			"sprinklers_temp":    [ { "mean": 68, "sd": 3 } ],
+			"sprinklers_density": [ { "mean": 0.0005, "sd": 0.0001 } ],
 			"sprinklers_not_broken": 0.96,
-			"sprinklers_rti": 50,
+			"sprinklers_rti": 50
+		} ,
+
+		"advanced": {
+
 
 			"nshevs": false,
 			"nshevs_activation_time": 0,
 
-			"hrrpua":	 { "min": 300    , "mode": 1000  , "max": 1300 }  ,
-			"hrr_alpha": { "min": 0.0029 , "mode": 0.047 , "max": 0.188 } ,
+			"hrrpua":	 [ { "min": 300    , "mode": 1000  , "max": 1300 }  ],
+			"hrr_alpha": [ { "min": 0.0029 , "mode": 0.047 , "max": 0.188 } ],
 
 			"windows": [ 
 				{ "min": -99999 , "max": -5    , "quarter": -5 , "full": 0.11 } ,
@@ -217,11 +217,11 @@ function read_json($json_path) { /*{{{*/
 				{ "min": 27     , "max": 99999 , "quarter": 0  , "full": 0.5 }
 			], 
 
-			"doors": { "E": 0.04, "C": 0.14, "D": 0.5 },
+			"doors": [ { "E": 0.04, "C": 0.14, "D": 0.5 } ],
 			"vvents_not_broken": 0.96,
 			"c_const": 8,
-			"pre_evac_fire_origin": { "mean": 59.85 , "sd": 1.48 } ,
-			"pre_evac_ordinary":    { "mean": 29.13 , "sd": 8.87 } ,
+			"pre_evac_fire_origin": [ { "mean": 59.85 , "sd": 1.48 } ],
+			"pre_evac_ordinary":    [ { "mean": 29.13 , "sd": 8.87 } ],
 
 			"evacuees_concentration": [
 				{ "room": "COR"  , "value": 20 } ,
@@ -230,10 +230,10 @@ function read_json($json_path) { /*{{{*/
 				{ "room": "HALL" , "value": 30 }
 			],
 
-			"evacuees_max_h_speed" : { "mean" : 120    , "sd" : 20 }    ,
-			"evacuees_max_v_speed" : { "mean" : 80     , "sd" : 20 }    ,
-			"evacuees_alpha_v"     : { "mean" : 0.706  , "sd" : 0.069 } ,
-			"evacuees_beta_v"      : { "mean" : -0.057 , "sd" : 0.015 } ,
+			"evacuees_max_h_speed" : [ { "mean" : 120    , "sd" : 20 }    ],
+			"evacuees_max_v_speed" : [ { "mean" : 80     , "sd" : 20 }    ],
+			"evacuees_alpha_v"     : [ { "mean" : 0.706  , "sd" : 0.069 } ],
+			"evacuees_beta_v"      : [ { "mean" : -0.057 , "sd" : 0.015 } ],
 			"fire_starts_in_room"  : 0.8
 
 		}
@@ -242,31 +242,7 @@ function read_json($json_path) { /*{{{*/
 	return $conf;
 }
 /*}}}*/
-function make_help() { /*{{{*/
-	$help=[];
-	$help["project_id"]              = ["project_id", "Some help" ]      ;
-	$help["scenario_id"]             = ["scenario_id", "We must write something here"] ;
-	$help["number_of_simulations"]   = ["number of simulations"   , "write me..."] ;
-	$help["simulation_time"]         = ["simulation time"         , "write me..."] ;
-	$help["indoor_temperature_min"]  = ["indoor_temperature_min"  , "write me..."] ;
-	$help["indoor_temperature_max"]  = ["indoor_temperature_max"  , "write me..."] ;
-	$help["outdoor_temperature_min"] = ["outdoor_temperature_min" , "write me..."] ;
-	$help["outoor_temperature_max"]  = ["outoor_temperature_max"  , "write me..."] ;
-	$help["indoor_pressure"]         = ["indoor_pressure"         , "write me..."] ;
-	$help["geometry_type"]           = ["geometry_type"           , "write me..."] ;
-	$help["material_ceiling"]        = ["material_ceiling"        , "write me..."] ;
-	$help["ceiling_thickness"]       = ["ceiling_thickness"       , "write me..."] ;
-	$help["material_floor"]          = ["material_floor"          , "write me..."] ;
-	$help["floor_thickness"]         = ["floor_thickness"         , "write me..."] ;
-	$help["material_wall"]           = ["material_wall"           , "write me..."] ;
-	$help["wall_thickness"]          = ["wall_thickness"          , "write me..."] ;
 
-	foreach($help as $k=>$v) { 
-		$help[$k][1]="<withHelp>?<help>$v[1]</help></withHelp>";
-	}
-	return $help;
-}
-/*}}}*/
 function geometry_type_droplist($in) {/*{{{*/
 	if(empty($in)) { $in='pl'; }
 	$select="<select name=post[geometry_type]>";
@@ -283,78 +259,77 @@ function geometry_type_droplist($in) {/*{{{*/
 	return $select;
 }
 /*}}}*/
-function material_ceiling_droplist($in) {/*{{{*/
+function material_droplist($i,$kk,$in) {/*{{{*/
 	if(empty($in)) { $in='pl'; }
-	$select="<select name=post[material_ceiling]>";
+	$select="<select name=post[material][$i][$kk]>";
 	$select.="<option value='$in'>$in</option>";
-	$select.="<option value=pl>pl</option>";
-	$select.="<option value=en>en</option>";
-	$select.="<option value=de>de</option>";
-	$select.="<option value=ru>ru</option>";
-	$select.="<option value=fr>fr</option>";
-	$select.="<option value=bg>bg</option>";
-	$select.="<option value=hu>hu</option>";
-	$select.="<option value=uk>uk</option>";
+	$select.="<option value=cegła>cegła</option>";
+	$select.="<option value=brick>brick</option>";
 	$select.="</select>";
 	return $select;
 }
 /*}}}*/
-function material_wall_droplist($in) {/*{{{*/
+function detectors_type_droplist($in) {/*{{{*/
 	if(empty($in)) { $in='pl'; }
-	$select="<select name=post[material_wall]>";
+	$select="<select name=post[detectors_type]>";
 	$select.="<option value='$in'>$in</option>";
-	$select.="<option value=pl>pl</option>";
-	$select.="<option value=en>en</option>";
-	$select.="<option value=de>de</option>";
-	$select.="<option value=ru>ru</option>";
-	$select.="<option value=fr>fr</option>";
-	$select.="<option value=bg>bg</option>";
-	$select.="<option value=hu>hu</option>";
-	$select.="<option value=uk>uk</option>";
+	$select.="<option value=X>X</option>";
+	$select.="<option value=Y>Y</option>";
 	$select.="</select>";
 	return $select;
 }
 /*}}}*/
-function material_floor_droplist($in) {/*{{{*/
+function sprinklers_type_droplist($in) {/*{{{*/
 	if(empty($in)) { $in='pl'; }
-	$select="<select name=post[material_floor]>";
+	$select="<select name=post[sprinklers_type]>";
 	$select.="<option value='$in'>$in</option>";
-	$select.="<option value=pl>pl</option>";
-	$select.="<option value=en>en</option>";
-	$select.="<option value=de>de</option>";
-	$select.="<option value=ru>ru</option>";
-	$select.="<option value=fr>fr</option>";
-	$select.="<option value=bg>bg</option>";
-	$select.="<option value=hu>hu</option>";
-	$select.="<option value=uk>uk</option>";
+	$select.="<option value=X>X</option>";
+	$select.="<option value=Y>Y</option>";
 	$select.="</select>";
 	return $select;
 }
 /*}}}*/
-function form($json_path=NULL) { /*{{{*/
-	$help=make_help();
-	$json=read_json($json_path);
-	echo "<form method=post>";
-	echo "<table>";
-	echo "<tr><th>param<th colspan=4>value<th>help";
-	$input="input type=text autocomplete=off";
-	$ceiling_thickness=" <td>ceiling_thickness<td> <input type=text size=2 name=post[ceiling_thickness] value=".$json['basic']['ceiling_thickness'].">";
-	$floor_thickness="   <td>floor_thickness  <td> <input type=text size=2 name=post[floor_thickness]   value=".$json['basic']['floor_thickness'].">";
-	$wall_thickness="    <td>wall_thickness   <td> <input type=text size=2 name=post[wall_thickness]    value=".$json['basic']['wall_thickness'].">";
-	unset($json['basic']['ceiling_thickness']);
-	unset($json['basic']['floor_thickness']);
-	unset($json['basic']['wall_thickness']);
-	foreach($json['basic'] as $k=>$v)   {
-		if($k=='project_id')            { echo "<tr><td>".$help[$k][0]."<td colspan=4>$v<td>".$help[$k][1]; }
-		else if($k=='scenario_id')      { echo "<tr><td>".$help[$k][0]."<td colspan=4>$v<td>".$help[$k][1]; }
-		else if($k=='geometry_type')    { echo "<tr><td>".$help[$k][0]."<td colspan=4>".geometry_type_droplist($v)."<td>".$help[$k][1]; }
-		else if($k=='material_ceiling') { echo "<tr><td>".$help[$k][0]."<td>".material_ceiling_droplist($v)."<td>$ceiling_thickness<td>".$help[$k][1]; }
-		else if($k=='material_wall')    { echo "<tr><td>".$help[$k][0]."<td>".material_wall_droplist($v)."<td>$wall_thickness<td>".$help[$k][1]; }
-		else if($k=='material_floor')   { echo "<tr><td>".$help[$k][0]."<td>".material_floor_droplist($v)."<td>$floor_thickness<td>".$help[$k][1]; }
-		else                            { echo "<tr><td>".$help[$k][0]."<td colspan=4><input type=text automplete=off size=10 name=post[$k] value='$v'><td>".$help[$k][1]; }
+
+function get_help($k) { # {{{
+	if(isset($_SESSION['help'][$k])) { 
+		return implode("&nbsp;", $_SESSION['help'][$k]);
+	} else {
+		return $k;
 	}
-	echo "</table>";
-	echo "<input type=submit name=update_conf value='submit'></form>";
+}
+/*}}}*/
+function form_material($arr) { #{{{
+	$z="";
+	$z="<table>";
+	$i=0;
+	foreach($arr as $k => $v) { 
+		$z.="<tr>";
+		foreach($v as $kk => $vv) { 
+			if(in_array($kk, array('ceiling', 'wall', 'floor') )) { 
+				$z.="<td>".get_help($kk)."<td>".material_droplist($i,$kk,$vv); 
+			} else {
+				$z.="<td>".get_help($kk)."<td><input size=2 type=text name=post[material][$i][$kk] value='$vv'><td>";
+			}
+		}
+		$i++;
+	}
+	$z.="</table>";
+	return $z;
+}
+/*}}}*/
+function form_plain_arr($key,$arr) { #{{{
+	$z="";
+	$z="<table>";
+	$i=0;
+	foreach($arr as $k => $v) { 
+		$z.="<tr>";
+		foreach($v as $kk => $vv) { 
+			$z.="<td>".get_help($kk)."<td><input size=8 type=text name=post[$key][$i][$kk] value='$vv'><td>";
+		}
+		$i++;
+	}
+	$z.="</table>";
+	return $z;
 }
 /*}}}*/
 function update() {/*{{{*/
@@ -362,14 +337,66 @@ function update() {/*{{{*/
 	dd($_POST['post']);
 }
 /*}}}*/
+
+function form($json_path=NULL) { /*{{{*/
+	$help=$_SESSION['help'];
+	$json=read_json($json_path);
+	dd($json);
+	echo "<form method=post>";
+	echo "<table>";
+	echo "<tr><th>params<th>values";
+	foreach($json['basic'] as $k=>$v) {
+
+		if($k=='project_id')               { echo "<tr><td>".get_help($k)."<td>$v"; }
+		else if($k=='scenario_id')         { echo "<tr><td>".get_help($k)."<td>$v"; }
+		else if($k=='geometry_type')       { echo "<tr><td>".get_help($k)."<td>".geometry_type_droplist($v); }
+		else if($k=='material')            { echo "<tr><td>$k<td>".form_material($v); }
+		else if($k=='detectors_type')      { echo "<tr><td>".get_help($k)."<td>".detectors_type_droplist($v); }
+		else if($k=='sprinklers_type')     { echo "<tr><td>".get_help($k)."<td>".sprinklers_type_droplist($v); }
+		else if($k=='indoor_temperature')  { echo "<tr><td>".get_help($k)."<td>".form_plain_arr($k,$v); }
+		else if($k=='outdoor_temperature') { echo "<tr><td>".get_help($k)."<td>".form_plain_arr($k,$v); }
+		else if($k=='detectors_temp')      { echo "<tr><td>".get_help($k)."<td>".form_plain_arr($k,$v); }
+		else if($k=='detectors_obscur')    { echo "<tr><td>".get_help($k)."<td>".form_plain_arr($k,$v); }
+		else if($k=='sprinklers_temp')     { echo "<tr><td>".get_help($k)."<td>".form_plain_arr($k,$v); }
+		else if($k=='sprinklers_density')  { echo "<tr><td>".get_help($k)."<td>".form_plain_arr($k,$v); }
+		else                               { echo "<tr><td>".get_help($k)."<td><input type=text automplete=off size=10 name=post[$k] value='$v'>"; }
+	}
+	echo "</table>";
+	echo "<input type=submit name=update_conf value='submit'></form>";
+}
+/*}}}*/
 function main() {/*{{{*/
 	if(empty($_SESSION['nn'])) { $_SESSION['nn']=new Aamks("Aamks") ; }
 	$_SESSION['nn']->htmlHead("Aamks");
+	$_SESSION['help']=make_help();
 	echo "<a class=blink href=apainter>apainter</a><br>";
 	update();
 	form();
 }
 /*}}}*/
 main();
+function make_help() { /*{{{*/
+	$help=[]                                                                                      ;
+	$help["project_id"]              = ["project_id"            , "Some help" ]                   ;
+	$help["scenario_id"]             = ["scenario_id"           , "We must write something here"] ;
+	$help["number_of_simulations"]   = ["number of simulations" , "write me..."]                  ;
+	$help["simulation_time"]         = ["simulation time"       , "write me..."]                  ;
+	$help["indoor_temperature"]      = ["indoor_temperature"    , "write me..."]                  ;
+	$help["outdoor_temperature"]     = ["outdoor_temperature"   , "write me..."]                  ;
+	$help["indoor_pressure"]         = ["indoor_pressure"       , "write me..."]                  ;
+	$help["ceiling"]				 = ["ceiling"               , "write me..."]                  ;
+	$help["floor"]          		 = ["floor"                 , "write me..."]                  ;
+	$help["wall"]           		 = ["wall"                  , "write me..."]                  ;
+	$help["detectors"]           	 = ["detectors"             , "write me..."]                  ;
+	$help["detectors_temp"]			 = ["detectors_temp"        , "write me..."] ;
+	$help["detectors_obscur"]		 = ["detectors_obscur"      , "write me..."] ;
+	$help["detectors_not_broken"]	 = ["detectors_not_broken"  , "write me..."] ;
+
+	foreach($help as $k=>$v) { 
+		$help[$k][1]="<withHelp>?<help>$v[1]</help></withHelp>";
+	}
+	return $help;
+}
+/*}}}*/
 
 ?>
