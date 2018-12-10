cd43a7545dbc068c42820d6db8cfe967ba811bd8
Karol Kreński <mimoohowy@gmail.com>
2018-12-10 23:40:55 +0100

    mimooh@d

--- geom/recast.py
+++
@@ -1,7 +0,0 @@
-wajig install libboost-python-dev
-git clone https://github.com/layzerar/recastlib.git
-cd recastlib
-cp -rf ./Recast\(Patched\)/Detour/ ./Recast/
-sed -i "s/libraries += .'boost_python3'./libraries += ['boost_python-py35']/" setup.py
-python3 setup.py install
-cd examples && python simpletest.py
