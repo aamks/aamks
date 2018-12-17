af98f8b80451ede3db2e72c7341963079c3c217f
Stanisław Łazowy <34373901+stanley998@users.noreply.github.com>
2018-12-17 19:49:22 +0100

    menaging projects

--- installer/install.sh
+++ installer/install.sh
@@ -117,31 +117,31 @@ END;
 \$\$ language 'plpgsql';
 
 ---}}}
-CREATE TABLE users (---{{{
-	id serial PRIMARY KEY, 
-    email character varying(50),
-    password character varying(70),
-    userName character varying(80),
-	editor varchar(10),
-	current_project_id varchar(100),
-	current_scenario_id varchar(100),
-	websocket_host varchar(100),
-	websocket_port varchar(20),
-	session_id varchar(100),
-	access_time timestamp,
-	access_ip varchar(100),
-	created timestamp default current_timestamp
-);
----}}}
-CREATE TABLE projects (---{{{
-	id serial PRIMARY KEY, 
-    user_id smallint,
-    name varchar(200),
-    description text,
-    category_id varchar(40),
-	modified timestamp default current_timestamp
-);
----}}}
+---CREATE TABLE users (---{{{
+---	id serial PRIMARY KEY, 
+---    email character varying(50),
+---    password character varying(70),
+---    userName character varying(80),
+---	editor varchar(10),
+---	current_project_id varchar(100),
+---	current_scenario_id varchar(100),
+---	websocket_host varchar(100),
+---	websocket_port varchar(20),
+---	session_id varchar(100),
+---	access_time timestamp,
+---	access_ip varchar(100),
+---	created timestamp default current_timestamp
+---);
+------}}}
+---CREATE TABLE projects (---{{{
+---	id serial PRIMARY KEY, 
+---    user_id smallint,
+---    name varchar(200),
+---    description text,
+---    category_id varchar(40),
+---	modified timestamp default current_timestamp
+---);
+------}}}
 CREATE TABLE categories (---{{{
 	id serial PRIMARY KEY, 
     user_id smallint,
@@ -152,17 +152,17 @@ CREATE TABLE categories (---{{{
 	modified timestamp default current_timestamp
 );
 ---}}}
-CREATE TABLE scenarios (---{{{
-	id serial PRIMARY KEY, 
-    project_id smallint,
-	name varchar(200),
-    fds_file text,
-	fds_object text,
-	ui_state text,
-    ac_file text,
-    ac_hash varchar(50),
-	modified timestamp default current_timestamp
-);
+---CREATE TABLE scenarios (---{{{
+---	id serial PRIMARY KEY, 
+---    project_id smallint,
+---	name varchar(200),
+---    fds_file text,
+---	fds_object text,
+---	ui_state text,
+---    ac_file text,
+---    ac_hash varchar(50),
+---	modified timestamp default current_timestamp
+---);
 ---}}}
 CREATE TABLE risk_scenarios (---{{{
 	id serial PRIMARY KEY, 
@@ -223,7 +223,38 @@ CREATE TABLE simulations ( ---{{{
 	inserted timestamp without time zone not null default now()
 );
 ---}}}
-
+CREATE TABLE users (---{{{
+	id serial PRIMARY KEY, 
+	google_id text,
+    userName text,
+    email text UNIQUE,
+    password text,
+	picture text,
+	active_project integer,
+	access_time timestamp,
+	activation_token text,
+	reset_token text,
+	modified timestamp,
+	created timestamp default current_timestamp
+);
+---}}}
+CREATE TABLE projects (---{{{
+	id serial PRIMARY KEY, 
+	user_id integer,
+	name text,
+	modified timestamp,
+	created timestamp default current_timestamp
+);
+---}}}
+CREATE TABLE scenarios (---{{{
+	id serial PRIMARY KEY, 
+	project_id integer,
+	name text,
+	modified timestamp,
+	created timestamp default current_timestamp
+);
+---}}}
+CREATE TRIGGER update_modified BEFORE UPDATE ON users FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 CREATE TRIGGER update_modified BEFORE UPDATE ON projects FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 CREATE TRIGGER update_modified BEFORE UPDATE ON scenarios FOR EACH ROW EXECUTE PROCEDURE update_modified_column();
 ---{{{ ALTERS
@@ -254,6 +285,22 @@ GRANT ALL ON SEQUENCE library_id_seq TO aamks;
 ALTER TABLE simulations OWNER TO aamks;
 GRANT ALL ON TABLE simulations TO aamks;
 GRANT ALL ON SEQUENCE simulations_id_seq TO aamks;
+
+
+ALTER TABLE users OWNER TO aamks;
+GRANT ALL ON TABLE users TO aamks;
+GRANT ALL ON SEQUENCE users_id_seq TO aamks;
+
+ALTER TABLE projects OWNER TO aamks;
+ALTER TABLE projects ADD CONSTRAINT projects_user_id FOREIGN KEY (user_id) REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE;
+GRANT ALL ON TABLE projects TO aamks;
+GRANT ALL ON SEQUENCE projects_id_seq TO aamks;
+
+ALTER TABLE scenarios OWNER TO aamks;
+ALTER TABLE scenarios ADD CONSTRAINT scenarios_project_id FOREIGN KEY (project_id) REFERENCES projects(id) ON UPDATE CASCADE ON DELETE CASCADE;
+GRANT ALL ON TABLE scenarios TO aamks;
+GRANT ALL ON SEQUENCE scenarios_id_seq TO aamks;
+
 ---}}}
 
 EOF
