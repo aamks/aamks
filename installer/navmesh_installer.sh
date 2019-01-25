61af690714b3c604c9e750020861b31f02688210
Karol Kreński <mimoohowy@gmail.com>
2019-01-25 17:52:07 +0100

    minor

--- installer/navmesh_installer.sh
+++ installer/navmesh_installer.sh
@@ -9,6 +9,21 @@
 # recast
 export GOPATH="$HOME/go"
 sudo apt-get --yes install golang git
+echo; echo;
+go version
+echo "If the above go version < 1.10 then you need to upgrade it yourself"
+echo "This might work for you"
+echo "sudo add-apt-repository ppa:gophers/archive"
+echo "sudo apt-get update"
+echo "sudo apt-get install golang-1.10-go"
+echo ""
+echo 'Then add to ~/.bashrc:' 
+echo 'export PATH="$PATH:/usr/lib/go-1.10/bin"'
+
+echo "Enter or ctrl+c"
+read
+
+
 echo; echo "Running go get -u github.com/arl/go-detour/cmd/recast..."
 go get -u github.com/arl/go-detour/cmd/recast
 sudo mv ~/go/bin/recast /usr/local/bin
