136ff6972e38f37b96c68174639f43fbd4993036
Karol Kreński <mimoohowy@gmail.com>
2019-01-27 15:22:25 +0100

    towards better integration

---
+++ gui/css/animator.js
@@ -0,0 +1,23 @@
+html, body { background-color: #000; margin: 0; color: #fff; overflow: hidden; height: 100%; font-size: 13px; }
+
+.scroll_rect { fill: #111111; }
+a:active, a:hover { text-decoration:none; color: #fff; }
+a:link, a:visited { color: #f80; text-decoration:none; }
+animation-setup-box { opacity: 0.9; background-color: #800; z-index:100; display: none; padding: 4px; color: #fff; border: 1px solid #f00; position: absolute; right:0px; top:40px; }
+canvas { border: 1px solid #444; width: 1600px; height: 800px; background: #000; } 
+canvas-mouse-coords { z-index:100; display: none; background-color: #444; font-size: 12px; padding: 4px; color: #fff; border: 1px solid #777; position: absolute; }
+close-left-menu-box { float: right; margin: -5px; }
+ered { padding: 6px; background: #800; color: #fff; }
+input  { padding:4px 8px; }
+input.buttonless-input { color: #f80; border: none; background: none; }
+left-menu-box     { left: 0px; background-color: #333; border: 1px solid #666; }
+left-menu-button  { color: #6af; font-size: 15px; font-weight: bold; margin-right:10px; }
+left-menu-button, close-left-menu-box { user-select: none; text-decoration: none; color:#6af; padding: 1px; cursor: pointer; font-size: 11px; }
+left-menu-button:hover, close-left-menu-box:hover { color: #c60 !important; }
+select { background-color: #222; margin: 0; color: #fff; }
+setup-box, left-menu-box { background-color: #800; z-index:100; padding: 10px; color: #fff; border: 1px solid #f00; position: absolute; top:40px; display: none; }
+show-animation-setup-box { cursor: default; float: right; }
+show-animation-setup-box:hover { opacity: 0.5; }
+table { border-spacing: 2px 2px; }
+td { border-bottom: 1px solid #a00; padding: 1px; color: #fff; }
+textarea, input, select { padding: 2px; margin: 4px; color: #fff; background: #311; border: 1px solid #866; }
