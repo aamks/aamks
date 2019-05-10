ebac1afc5db52537ec68825ff4ba2dcad3672762
Karol Kreński <mimoohowy@gmail.com>
2019-05-11 00:59:17 +0200

    multifloor broken for now, but static.json should be fixed

--- include.py
+++ include.py
@@ -266,27 +266,25 @@ class Vis:# {{{
         self.conf=self.json.read("{}/conf.json".format(os.environ['AAMKS_PROJECT']))
 
         self._static_floors=OrderedDict()
-        self._static_world2d=OrderedDict()
         self._js_make_floors_and_meta()
         self._js_make_rooms()
         self._js_make_doors()
         self._js_make_obstacles()
         self._js_make_dd_geoms()
 
+        self.global_meta=JSON.readdb("global_meta")
+        if self.global_meta['multifloor_building']==1:
+            self._static_world2d=OrderedDict()
+            self._js_make_floors_and_meta_world2d()
+            self._js_make_rooms_world2d()
+            self._js_make_doors_world2d()
+            self._js_make_obstacles_world2d()
+            self._js_make_dd_geoms_world2d()
+
         if 'fire_origin' not in params:
             params['fire_origin']=self._js_vis_fire_origin()
 
         self._save(params)
-# }}}
-    def _js_make_floors_and_meta(self):# {{{
-        ''' Animation meta tells how to scale and translate canvas view '''
-        
-        for floor,meta in json.loads(self.s.query("SELECT * FROM floors_meta")[0]['json']).items():
-            self._static_floors[floor]=OrderedDict()
-            self._static_floors[floor]['floor_meta']=meta
-
-        meta=json.loads(self.s.query("SELECT * FROM world2d_meta")[0]['json'])
-        self._static_world2d['floor_meta']=meta
 # }}}
     def _make_poly(self,i):# {{{
         points=[]
@@ -296,6 +294,19 @@ class Vis:# {{{
         points.append(OrderedDict([('x', i['x0']), ("y", i['y1'])]))
         return points
 # }}}
+    def _js_make_floors_and_meta(self):# {{{
+        ''' Animation meta tells how to scale and translate canvas view '''
+        
+        for floor,meta in json.loads(self.s.query("SELECT * FROM floors_meta")[0]['json']).items():
+            self._static_floors[floor]=OrderedDict()
+            self._static_floors[floor]['floor_meta']=meta
+# }}}
+    def _js_make_floors_and_meta_world2d(self):# {{{
+        ''' Animation meta tells how to scale and translate canvas view '''
+        
+        meta=json.loads(self.s.query("SELECT * FROM world2d_meta")[0]['json'])
+        self._static_world2d['floor_meta']=meta
+# }}}
     def _js_make_rooms(self):# {{{
         ''' Data for rooms. '''
 
@@ -304,6 +315,9 @@ class Vis:# {{{
             for i in self.s.query("SELECT name,x0,y0,x1,y1,type_sec,room_enter FROM aamks_geom WHERE floor=? AND type_pri='COMPA'", (floor,)):
                 points=self._make_poly(i)
                 self._static_floors[floor]['rooms'].append(OrderedDict([ ('name', i['name']), ('type_sec', i['type_sec']), ('room_enter', i['room_enter']), ('points', points)]))
+# }}}
+    def _js_make_rooms_world2d(self):# {{{
+        ''' Data for rooms. '''
 
         for floor in ['world2d']:
             self._static_world2d['rooms']=[]
@@ -320,6 +334,9 @@ class Vis:# {{{
             for i in self.s.query("SELECT name,x0,y0,x1,y1,type_sec FROM aamks_geom WHERE floor=? AND type_tri='DOOR' AND type_sec != 'HOLE'", (floor,)):
                 points=self._make_poly(i)
                 self._static_floors[floor]['doors'].append(OrderedDict([ ('name', i['name']), ('type_sec', i['type_sec']), ('points', points)]))
+# }}}
+    def _js_make_doors_world2d(self):# {{{
+        ''' Data for doors. '''
 
         for floor in ['world2d']:
             self._static_world2d['doors']=[]
@@ -341,6 +358,19 @@ class Vis:# {{{
                     self._static_floors[floor]['obstacles'].append({'points': [ OrderedDict([('x', o[0]),('y', o[1])]) for o in obst[:4] ]})
                 self._static_floors[floor]['obstacles'].append({'points': [ OrderedDict([('x', o[0]),('y', o[1])]) for o in _json['fire_obstacle'][:4] ], 'type':'fire_obstacle'})
 
+        except:
+            empty_obst=[ OrderedDict([('x', o[0]),('y', o[1])]) for o in [(0,0),(0,0),(0,0),(0,0)] ]
+            for floor in self._static_floors.keys():
+                self._static_floors[floor]['obstacles']=[]
+                self._static_floors[floor]['obstacles'].append({'points': empty_obst })
+# }}}
+    def _js_make_obstacles_world2d(self):# {{{
+        ''' 
+        Data for obstacles. It may happen that geom.py was interrupted before
+        obstacles were created, so we produce a 0 size obstacle in try/except. 
+        '''
+
+        try:
             _json=JSON.readdb("world2d_obstacles")
             obstacles=_json['points']
             self._static_world2d['obstacles']=[]
@@ -349,10 +379,6 @@ class Vis:# {{{
 
         except:
             empty_obst=[ OrderedDict([('x', o[0]),('y', o[1])]) for o in [(0,0),(0,0),(0,0),(0,0)] ]
-            for floor in self._static_floors.keys():
-                self._static_floors[floor]['obstacles']=[]
-                self._static_floors[floor]['obstacles'].append({'points': empty_obst })
-
             self._static_world2d['obstacles'].append({'points': empty_obst })
 # }}}
     def _js_make_dd_geoms(self):# {{{
@@ -365,7 +391,15 @@ class Vis:# {{{
         f=self.json.read("{}/dd_geoms.json".format(os.environ['AAMKS_PROJECT']))
         for floor in self._static_floors.keys():
             self._static_floors[floor]['dd_geoms']=f[floor]
+# }}}
+    def _js_make_dd_geoms_world2d(self):# {{{
+        ''' 
+        dd_geoms are initialized in geom.py. Those are optional extra
+        rectangles, points, lines and circles that are written to on top of our
+        geoms. Useful for debugging.
+        '''
 
+        f=self.json.read("{}/dd_geoms.json".format(os.environ['AAMKS_PROJECT']))
         self._static_world2d['dd_geoms']=f['world2d']
 # }}}
     def _js_vis_fire_origin(self):# {{{
@@ -403,7 +437,7 @@ class Vis:# {{{
         '''
 
         vis_dir="{}/workers".format(os.environ['AAMKS_PROJECT']) 
-        if self._static_world2d['floor_meta']['multifloor']==1:
+        if self.global_meta['multifloor_building']==1:
             self._static_floors['world2d']=self._static_world2d
         self.json.write(self._static_floors, '{}/static.json'.format(vis_dir)) 
 
