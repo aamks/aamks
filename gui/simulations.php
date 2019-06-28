edc51baafe1c4bcea555e5ad26b0a1be20e4c1f9
akrasuski <adam.p.krasuski@gmail.com>
2019-06-28 14:25:44 +0200

    charts placement

--- gui/simulations.php
+++ gui/simulations.php
@@ -10,9 +10,9 @@ function listing() {/*{{{*/
 /*}}}*/
 function status() {/*{{{*/
 	extract($_SESSION['main']);
-	$r=$_SESSION['nn']->query("SELECT count(*) AS finished FROM simulations where project = $project_id and dcbe_time is not null");
+	$r=$_SESSION['nn']->query("SELECT count(*) AS finished FROM simulations WHERE project = $project_id and dcbe_time is not null");
 	$finished=$r[0]['finished'];
-	$r=$_SESSION['nn']->query("SELECT count(*) AS total FROM simulations where project = $project_id");
+	$r=$_SESSION['nn']->query("SELECT count(*) AS total FROM simulations WHERE project = $project_id");
 	$total=$r[0]['total'];
 	echo "<br>Complete: $finished of $total &nbsp; &nbsp; <wheat>ctrl+r to refresh</wheat>";
 }
@@ -29,12 +29,15 @@ function show_pictures() {/*{{{*/
 	$z=shell_exec("$cmd");
 
 	echo "<br>$z";
-	$pictures_list = array('pie_fault', 'lossesdead', 'lossesheavy', 'losseslight', 'lossesneglegible', 'ccdf', 'dcbe', 'wcbe', 'height', 'hgt_cor', 'temp', 'vis', 'vis_cor', 'tree', 'tree_steel');
+	$counter = 1;
+	$pictures_list = array(array('pie_fault','title'), array('lossesdead','title'), array('lossesheavy','title'), array('losseslight','title'), array('lossesneglegible','title'), array('ccdf','title'), array('dcbe','title'), array('wcbe','title'), array('height','title'), array('hgt_cor','title'), array('temp','title'), array('vis','title'), array('vis_cor','title'), array('tree','title'), array('tree_steel','title'));
 	foreach($pictures_list as $picture) {
-		$file=$f."/picts/".$picture.".png";
+		$file=$f."/picts/".$picture[0].".png";
 		$size_info=getimagesize($file);
 		$data64=shell_exec("base64 $file");
-		echo "<img style='display:block; width:".$size_info[0]."px;height:".$size_info[1]."px;' src='data:image/png;base64, $data64'/>";
+		echo "<img class='results-pictures' style='width:".$size_info[0]."px;height:".$size_info[1]."px;' src='data:image/png;base64, $data64'/>";
+		echo "<p>Figure ". $counter .". <strong>".$picture[1]."</strong></p>";
+		$counter += 1;
 	}
 }
 /*}}}*/
