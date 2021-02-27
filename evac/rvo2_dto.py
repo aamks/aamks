df6381f4e65046fdba6673d60385b1e7e2acba39
AleksanderKamienski <33099769+AleksanderKamienski@users.noreply.github.com>
2021-02-27 11:06:13 +0100

    Update rvo2_dto.py
    sum misspel

--- evac/rvo2_dto.py
+++ evac/rvo2_dto.py
@@ -323,7 +323,7 @@ class EvacEnv:
 
     def group_data(self):
         unique_cell_numbers = set(map(lambda x:x['cell_number'], self.position_fed_to_insert))
-        self.fed_growth_grouped_by_cell = [{'sum':sumj([row['fed_growth'] for row in self.position_fed_to_insert if row['cell_number']==cell_number]),'count':len([row['fed_growth'] for row in self.position_fed_to_insert if row['cell_number']==cell_number]), 'cell_number':cell_number} for cell_number in unique_cell_numbers]
+        self.fed_growth_grouped_by_cell = [{'sum':sum([row['fed_growth'] for row in self.position_fed_to_insert if row['cell_number']==cell_number]),'count':len([row['fed_growth'] for row in self.position_fed_to_insert if row['cell_number']==cell_number]), 'cell_number':cell_number} for cell_number in unique_cell_numbers]
 
     def find_proper_cell_and_append(self, x, y, fed_growth, floor):
         for j in range(len(self.position_fed_tables_information)):
