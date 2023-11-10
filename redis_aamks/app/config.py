a18da34f1ba054780fa3c598d43fb977915e0984
zarooba <zawistowskihubert01@gmail.com>
2023-11-10 08:32:31 +0100

    password security

--- redis_aamks/app/config.py
+++ redis_aamks/app/config.py
@@ -1,6 +1,9 @@
 import os
 
 redis_host = os.environ['AAMKS_SERVER']
+print(os.environ)
+print(os.environ['AAMKS_REDIS_PASS'])
+
 redis_port = 6379
 redis_db_number = 0
 redis_password = os.environ['AAMKS_REDIS_PASS']
