0c1586752ac75526c3ea1bdbc510b584576171c1
zarooba <zawistowskihubert01@gmail.com>
2023-11-17 12:16:41 +0100

    worker env in mnt

--- redis_aamks/worker/config.py
+++ redis_aamks/worker/config.py
@@ -1,7 +1,13 @@
 import os
+import sys
+sys.path.append('/mnt/aamks_users')
+try:
+    from worker_env import *
+except Exception as e:
+    print("Have you properly configured your mnt/aamks_users/worker_env.py?")
 
-redis_host = os.environ['AAMKS_SERVER']
+redis_host = AAMKS_SERVER
 redis_port = 6379
 redis_db_number = 0
-redis_password = os.environ['AAMKS_REDIS_PASS']
+redis_password = AAMKS_REDIS_PASS
 redis_queue_name = "aamks_queue"
