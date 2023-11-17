a2a7f1bb47dafc83f28a829b2e7e84fcfb719a72
zarooba <zawistowskihubert01@gmail.com>
2023-11-17 14:11:16 +0100

    simplify mnt worker_env

--- redis_aamks/worker/config.py
+++ redis_aamks/worker/config.py
@@ -5,17 +5,7 @@ sys.path.append('/mnt/aamks_users')
 try:
     from worker_env import AAMKS_SERVER, AAMKS_REDIS_PASS
 except ImportError:
-    print("Error importing from worker_env. Using os.environ instead.")
-    try:
-        if os.environ['AAMKS_SERVER']:
-            redis_host = os.environ['AAMKS_SERVER']
-            redis_password = os.environ('AAMKS_REDIS_PASS')
-        else:
-            print("AAMKS_SERVER not properly configured or doesn't start with '127'")
-    except Exception as e:
-        print("Bad configuration in worker/config")
-        redis_host = ''
-        redis_password = ''
+    print("Error importing from worker_env.")
 else:
     redis_host = AAMKS_SERVER
     redis_password = AAMKS_REDIS_PASS
