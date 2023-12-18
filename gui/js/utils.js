7719fb17ef17a97afdeb374233e69a2b454679dc
Mateusz Mackiewicz <mateuszmackiewicz97@gmail.com>
2023-12-18 11:57:07 +0100

    added form validation

--- gui/js/utils.js
+++ gui/js/utils.js
@@ -106,43 +106,6 @@ function amsg(r) {//{{{
 	}
 }
 //}}}
-function searchTable() {
-	var input, filter, found, table, tr, td, i, j;
-	input = document.getElementById("search");
-	filter = input.value.toUpperCase();
-	table = document.getElementById("sim");
-	tr = table.getElementsByTagName("tr");
-	for (i = 0; i < tr.length; i++) {
-		td = tr[i].getElementsByTagName("td");
-		for (j = 0; j < td.length; j++) {
-			if (td[j].innerHTML.toUpperCase().indexOf(filter) > -1) {
-				found = true;
-			}
-		}
-		if (found) {
-			tr[i].style.display = "";
-			found = false;
-		} else {
-			tr[i].style.display = "none";
-		}
-	}
-}
-function sortTable(){
-	document.getElementById("animcheck").checked="checked";
-	document.getElementById('trAnim').style.cursor = 'pointer';
-	const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
-
-	const comparer = (idx, asc) => (a, b) => ((v1, v2) => 
-	v1 !== '' && v2 !== '' && !isNaN(v1) && !isNaN(v2) ? v1 - v2 : v1.toString().localeCompare(v2)
-	)(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
-
-	document.querySelectorAll('th').forEach(th => th.addEventListener('click', (() => {
-	const table = th.closest('table');
-	Array.from(table.querySelectorAll('tr:not(thead tr)'))
-		.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-		.forEach(tr => table.appendChild(tr) );
-	})));
-}
 function scenario_changer() {//{{{
 	$("body").on("change", "#choose_scenario", function() {
 		$.post('/aamks/ajax.php?ajaxChangeActiveScenario', {'ch_scenario':$(this).val() }, function (json) { 
