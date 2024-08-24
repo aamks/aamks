1854260afb8d9740535b2325816b1b56a46497a2
Mateusz Mackiewicz <mateuszmackiewicz97@gmail.com>
2024-08-24 17:29:15 +0200

    removed aamks_users dependency

--- gui/animator/anim.php
+++ gui/animator/anim.php
@@ -1,12 +1,12 @@
 <?php
 session_name('aamks');
-require_once("../inc.php"); 
+require_once("../inc.php");
 
 function site() {/*{{{*/
 	$colors=json_decode(file_get_contents("inc.json"),1)['aamksGeoms'];
 	$doses="
 	<div style='display: flex'>
-		<a target=blank_ href=https://www.google.com/search?q=fractional+effective+dose>FED</a>: 
+		<a target=blank_ href=https://www.google.com/search?q=fractional+effective+dose>FED</a>:
 		<div style='margin:5px; width:10px; height:10px; border-radius: 10px; border: 2px solid ".$colors['color_N']['stroke']."; background-color: ".$colors['color_N']['c']."; color: #000'></div>Neutral
 		<div style='margin:5px; width:10px; height:10px; border-radius: 10px; border: 2px solid ".$colors['color_L']['stroke']."; background-color: ".$colors['color_L']['c']."; color: #000'></div>Low
 		<div style='margin:5px; width:10px; height:10px; border-radius: 10px; border: 2px solid ".$colors['color_M']['stroke']."; background-color: ".$colors['color_M']['c']."; color: #000'></div>Medium
@@ -24,13 +24,13 @@ function site() {/*{{{*/
 		<close-right-menu-box><img src=/aamks/css/close.svg></close-right-menu-box><br>
 		<table>
 			<tr><td><letter>Space</letter><td>pause/unpause animation
-			<tr><td>Highlight           <td><highlight-geoms></highlight-geoms> 
-			<tr><td>Style               <td><change-style></change-style> 
-			<tr><td>Labels size         <td><size-labels></size-labels> 
-			<tr><td>Walls size          <td><size-walls></size-walls> 
-			<tr><td>Doors size          <td><size-doors></size-doors> 
-			<tr><td>Evacuee&nbsp;radius <td><radius-evacuee></radius-evacuee> 
-			<tr><td>Vectors&nbsp;size   <td><size-velocities></size-velocities> 
+			<tr><td>Highlight           <td><highlight-geoms></highlight-geoms>
+			<tr><td>Style               <td><change-style></change-style>
+			<tr><td>Labels size         <td><size-labels></size-labels>
+			<tr><td>Walls size          <td><size-walls></size-walls>
+			<tr><td>Doors size          <td><size-doors></size-doors>
+			<tr><td>Evacuee&nbsp;radius <td><radius-evacuee></radius-evacuee>
+			<tr><td>Vectors&nbsp;size   <td><size-velocities></size-velocities>
 			<tr><td>Speed               <td><animation-speed></animation-speed>
 			<tr><td colspan=2>$doses
 		</table>
