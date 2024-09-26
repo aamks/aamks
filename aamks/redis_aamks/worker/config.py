c1443bc4f0b7e3836a80a3900433d5947ba965af
kowalskiw <kowalskiwojciech986@gmail.com>
2024-09-26 15:01:29 +0200

    restore master project architecture

--- aamks/redis_aamks/worker/config.py
+++
@@ -1,7 +0,0 @@
-import os
-redis_host = os.environ['AAMKS_SERVER']
-redis_password = os.environ['AAMKS_REDIS_PASS']
-main_path = "/".join(os.environ['AAMKS_PROJECT'].split('/')[:-3])
-redis_port = 6379
-redis_db_number = 0
-redis_queue_name = "aamks_queue"
